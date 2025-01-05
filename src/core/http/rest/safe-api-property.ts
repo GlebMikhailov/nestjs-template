4a6926d539c655df6aa3a4111d0b89bd37b37fba
developerandr <developerandr9@gmail.com>
2025-01-06 01:15:41 +0300

    refactoring: Change name to SafeApiProperty

--- src/core/http/rest/safe-api-property.ts
+++ src/core/http/rest/safe-api-property.ts
@@ -2,7 +2,11 @@ import { applyDecorators } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
 import { ApiPropertyOptions } from '@nestjs/swagger/dist/decorators/api-property.decorator';
 import { Expose } from 'class-transformer';
+import { ExposeOptions } from 'class-transformer/types/interfaces';
 
-export function SafeApiProperty(options: ApiPropertyOptions) {
-    return applyDecorators(ApiProperty(options), Expose());
+export function SafeApiProperty(
+    apiPropertyOptions: ApiPropertyOptions,
+    exposeOptions?: ExposeOptions,
+) {
+    return applyDecorators(ApiProperty(apiPropertyOptions), Expose(exposeOptions));
 }
